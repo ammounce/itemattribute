7e788db7bf05147aa040fdb11e4f2a5d6d8c6611
ammounce <ammounce@gmail.com>
2024-09-15 14:01:31 -0600

    fix: fixed version?

--- src/itemattribute/__init__.py
+++ src/itemattribute/__init__.py
@@ -1,8 +1,11 @@
 from .item_attribute import ItemAttribute
+
+import importlib.resources
 import json
 import  os
 
+
 dir = os.path.dirname(os.path.abspath(__file__))
 
-with open(dir + '/version.json') as file:
-    __version__ = json.load(file)
+with importlib.resources.open_text("itemattribute", "version.json") as file:
+    __version__ = json.load(file)['version']
